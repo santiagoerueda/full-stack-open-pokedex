39adc5d501d664f5ea52cf3fa0b7bd16bb264bcb
santiagorueda@live.com.ar <santiagorueda@live.com.ar>
2023-08-06 10:44:59 -0300

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,2 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+const { defineConfig } = require('cypress')
+module.exports = defineConfig()
